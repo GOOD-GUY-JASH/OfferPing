b476f5d000ae7e4aaaa740f0a8858481dd9a0476
GOOD-GUY-JASH <aibyom1601@gmail.com>
2026-07-07 00:01:32 +0530

    Added product API

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+products = []
 
 @app.route("/")
 def home():
@@ -9,6 +11,24 @@ def home():
         "app": "OfferPing",
         "version": "1.0"
     }
+
+@app.route("/save-product", methods=["POST"])
+def save_product():
+    data = request.get_json()
+
+    products.append({
+        "url": data.get("url"),
+        "target_price": data.get("target_price")
+    })
+
+    return jsonify({
+        "success": True,
+        "message": "Product saved!"
+    })
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    return jsonify(products)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
