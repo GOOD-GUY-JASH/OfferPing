3f4344e30b24ad4f58aad89120a7baf70e80ee11
GOOD-GUY-JASH <aibyom1601@gmail.com>
2026-07-07 16:11:23 +0530

    Test Playwright

--- checker.py
+++ checker.py
@@ -1,6 +1,10 @@
-import json
+from playwright.sync_api import sync_playwright
 
-with open("products.json", "r") as f:
-    products = json.load(f)
+url = "https://dl.flipkart.com/dl/hp-victus-intel-core-i5-14th-gen-14450hx-24-gb-512-gb-ssd-windows-11-home-6-graphics-nvidia-geforce-rtx-4050-15-fa2382tx-gaming-laptop/p/itmc8b9497c77521"
 
-print(products)
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto(url, wait_until="networkidle")
+    print(page.title())
+    browser.close()
